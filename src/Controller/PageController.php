9303d39b0ea9960497486f27482592a281714de2
souhayla2024 <souhaila.sakly@xeniusconsulting.com>
2025-10-26 15:47:15 +0100

    feat/work-on-first-step

---
+++ src/Controller/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('login.html.twig');
+    }
+}
---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(): Response
+    {
+        return $this->render('products/index.html.twig');
+    }
+
+    #[Route('/products/{id}', name: 'app_single_product')]
+    public function single(int $id): Response
+    {
+        return $this->render('products/single-product.html.twig', [
+            'product_id' => $id
+        ]);
+    }
+}
